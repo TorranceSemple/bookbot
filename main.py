8539cc7d99bbce4beab7763273e4efe1226dad97
Torrance Semple <torrancesemple@gmail.com>
2025-06-07 23:55:39 +0700

    frankenstein

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("greetings boots")
+def get_book_text(path_to_file):
+    with open(path_to_file) as f:
+        file_contents = f.read() # f is a file object
+    return file_contents
+    
+def main():
+    print(get_book_text("/Users/torrancesemple/workspace/github.com/TorranceSemple/bookbot/books/frankenstein.txt"))
+
+if __name__ == "__main__":
+    main()
